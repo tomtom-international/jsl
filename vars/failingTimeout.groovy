9d2f8f6054cccf0fbe6b875b78a1735eeb1adf2e
Eugene Davis <eugene.davis@tomtom.com>
2019-04-04 14:47:16 +0200

    Correct copyright header

--- vars/failingTimeout.groovy
+++ vars/failingTimeout.groovy
@@ -1,12 +1,20 @@
-/* Copyright (c) 2019 - 2019 TomTom N.V. All rights reserved.
+/*
+ * Copyright (C) 2012-2019, TomTom (http://tomtom.com).
  *
- * This software is the proprietary copyright of TomTom N.V. and its subsidiaries and may be
- * used for internal evaluation purposes or commercial use strictly subject to separate
- * licensee agreement between you and TomTom. If you are the licensee, you are only permitted
- * to use this Software in accordance with the terms of your license agreement. If you are
- * not the licensee then you are not authorised to use this software in any manner and should
- * immediately return it to TomTom N.V.
- * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * /
+
+ /*
  * failingTimeout()
  * Description: Wraps a timeout call and throws an error rather than an abort if the timeout is exceeded.
  * Modified from https://support.cloudbees.com/hc/en-us/articles/226554067/comments/360000870712

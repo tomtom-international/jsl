cc11d3fb908d2566af240975a0c5cd3e41085971
Martin Migasiewicz <616250+martinm82@users.noreply.github.com>
2019-05-03 13:11:28 +0200

    fix(artifactory-upload): change order of arguments and update doc (#12)
    * fix: change order of arguments and update doc
    
    * fixup: add missing closing parenthis

--- vars/artifactoryUploadDebianPackage.groovy
+++ vars/artifactoryUploadDebianPackage.groovy
@@ -1,16 +1,16 @@
 /**
 * artifactoryUploadDebianPackage
-* Uploads the specified debian package to artifactory. Defaults to the navkit-apt-local repo in artifactory.navkit-pipeline.tt3.com
-* Requires that a username-password credential exist under the name "artifactory-apt-local" with valid credentials to artficatory.
+* Uploads the specified debian package to artifactory. 
+* Requires that a username-password credential exist under the name "artifactory-apt-local" with valid credentials to Artifactory.
 * filename - String deb package filename
 * ubuntuVersion - String Version of Ubuntu package is for (i.e. trusty or xenial)
 * path - String path to directory containing deb packages
 * credentialsId - String credentials identifier
-* artifactoryUrl - String URL of Artifactory
-* artifactoryRepo - String artifactory debian repo
+* artifactoryUrl - String URL of Artifactory (eg. artifactory.your-org.com)
+* artifactoryRepo - String Artifactory Debian repository
 * arch - String architecture string
 */
-def call(String filename, String ubuntuVersion, String path = ".", String credentialsId "artifactory-apt-local", String artifactoryUrl, String artifactoryRepo = "apt-local", String arch = "amd64") {
+def call(String filename, String ubuntuVersion, String path, String artifactoryUrl, String artifactoryRepo = "apt-local", String credentialsId = "artifactory-apt-local", String arch = "amd64") {
     String aptDistros = "deb.component=main;deb.distribution=$ubuntuVersion"
     withCredentials([usernamePassword(credentialsId: "$credentialsId", usernameVariable: "username", passwordVariable: "password")]) {
         sh "cd $path && curl --fail --user \"${username}:${password}\" -X PUT \"https://${artifactoryUrl}/artifactory/${artifactoryRepo}/pool/${filename};deb.architecture=${arch};${aptDistros}\" -T \"${filename}\""

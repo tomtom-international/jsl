288fa6a469006e03a3ffbb9b35635efc9fce4a8d
Swathi Srirangapatna Ashok Kumar <58590949+swathisa@users.noreply.github.com>
2020-01-09 13:28:13 +0100

    fix: push docs specific tag only in deploy docs stage (#22)

--- vars/pythonSetupPyPipeline.groovy
+++ vars/pythonSetupPyPipeline.groovy
@@ -245,7 +245,7 @@ def call(Map pipelineParams) {
                   }
                   sh "ghp-import -m \"Documentation update to $moduleVersion\" -p -b docs build/sphinx/html"
                   sh "git tag docs-$moduleVersion docs"
-                  sh "git push origin docs --tags"
+                  sh "git push origin docs docs-$moduleVersion"
                 }
               }
             }
